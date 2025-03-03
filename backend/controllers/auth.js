734bcc2987caa45c878bc9eb03d0574d60b28232
Sohila Metwaly <103280614+sohilametwaly@users.noreply.github.com>
2025-03-03 17:43:32 +0200

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,8 +8,8 @@ export const signIn = async (req, res) => {
   const { email, password } = req.body;
   try {
     const validationRes = validationResult(req);
-    if (validationRes.length > 0) {
-      return res.status(400).json({ message: validationRes });
+     if (validationRes.errors.length > 0) {
+      return res.status(400).json({ message: validationRes.errors });
     }
     console.log(email, password);
     console.log(validationRes);
@@ -41,8 +41,8 @@ export const signUp = async (req, res) => {
   const { name, email, password, isCompany } = req.body;
   try {
     const validationRes = validationResult(req);
-    if (validationRes.length > 0) {
-      return res.status(400).json({ message: validationRes });
+    if (validationRes.errors.length > 0) {
+      return res.status(400).json({ message: validationRes.errors });
     }
 
     const user = await User.findOne({ email });
